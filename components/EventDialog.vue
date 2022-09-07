bd5fd21531dccca317a35ec87f12901da61b45aa
Akash N O <akash@armino.in>
2022-09-07 15:33:18 +0530

    onam image fix

--- components/EventDialog.vue
+++ components/EventDialog.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-if="showModal" class="overflow-x-hidden overflow-y-auto bg-black bg-opacity-70 fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex" @click="toggleModal()">
       <div class="relative">
-            <img src="static/onam.jpeg" class="mx-auto md:w-1/2 w-100" height="200px"/>
+            <img src="/onam.jpeg" class="mx-auto md:w-1/2 w-100" height="200px"/>
             <div class="absolute -top-2 right-72">
             <p @click="toggleModal()" class="text-xl w-8 h-8 rounded-full  flex justify-center align-middle  text-black bg-white  cursor-pointer">x</p>
             </div>
